60d3505c13b0ebffc9021369f74be559a8e1d3b7
ChandanaJilla <84612358+ChandanaJilla@users.noreply.github.com>
2023-01-11 09:33:15 +0530

    Update HandDataCollecter.py

--- HandDataCollecter.py
+++ HandDataCollecter.py
@@ -30,15 +30,9 @@ def GetRelativeDistance(StandardLength, p1, p2):
     return PointDistance/StandardLength
 
 def ImageToDistanceData(image, hands):
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
     image.flags.writeable = False
     results = hands.process(image)
-
-    # Draw the hand annotations on the image.
 
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -80,24 +74,24 @@ def ImageToDistanceData(image, hands):
 
                 point = Hand_Frame_Data[Position_Layers[layerindex][sublayerindex]]
 
-                if layerindex > 1: #get distance from one below, make exception for wrist
+                if layerindex > 1: 
                     BelowLayerPoint = Hand_Frame_Data[Position_Layers[layerindex-1][sublayerindex]]
                     
                     RelativeLength = GetRelativeDistance(StandardLength, point, BelowLayerPoint)
-                    #cv2.putText(image,  str(round(RelativeLength, 2)), (point), 0, 0.5, 255)
+                    
 
                     colour = (RelativeLength/0.3) * 255
                     if colour > 255: colour = 255
-                    #cv2.line(image, (point), (BelowLayerPoint), (0, colour, 0), thickness=3)
+                   
 
                     DistanceData.append(RelativeLength)
                 
-                if layerindex == 4: #get distance from fingertip to palm
+                if layerindex == 4: 
                     RelativeLength = GetRelativeDistance(StandardLength, point, Hand_Frame_Data[0])
 
                     colour = (RelativeLength/0.6) * 255
                     if colour > 255: colour = 255
-                    #cv2.line(image, (point), (Hand_Frame_Data[0]), (0, colour, 0), thickness=3)
+                    
 
                     DistanceData.append(RelativeLength)
 
@@ -105,11 +99,7 @@ def ImageToDistanceData(image, hands):
                         NeighbourPoint = Hand_Frame_Data[Position_Layers[layerindex][sublayerindex+1]]
                         RelativeLength = GetRelativeDistance(StandardLength, point, NeighbourPoint)
 
-                        #cv2.putText(image,  str(round(RelativeLength, 2)), (point), 0, 0.5, 255)
-
-                        #colour = (RelativeLength/0.6) * 255
-                        #if colour > 255: colour = 255
-                        #cv2.line(image, (point), (NeighbourPoint), (0, colour, 0), thickness=3)
+                  
 
                         DistanceData.append(RelativeLength)
 
@@ -117,9 +107,7 @@ def ImageToDistanceData(image, hands):
                     if sublayerindex > 0:
                         RelativeLength = GetRelativeDistance(StandardLength, point, Hand_Frame_Data[4])
 
-                        #colour = (RelativeLength/0.6) * 255
-                        #if colour > 255: colour = 255
-                        #cv2.line(image, (point), (Hand_Frame_Data[4]), (0, colour, 0), thickness=3)
+                       
 
                         DistanceData.append(RelativeLength)
                     
@@ -137,31 +125,29 @@ if __name__ == '__main__':
     PKL_PATH = os.path.join(os.path.join((os.path.dirname(os.path.realpath('__file__'))),"ASL Pickles"),(ChosenLetter + '-dataset.pkl'))
     
     timeline = []
-    # For webcam input:
     cap = cv2.VideoCapture(0)
     with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
         while cap.isOpened():
             success, image = cap.read()
             if not success:
                 print("Ignoring empty camera frame.")
-                # If loading a video, use 'break' instead of 'continue'.
                 continue
             
             ImageData = ImageToDistanceData(image, hands)
             DistanceData = ImageData['Distance-Data']
             image = ImageData['image']
 
-            if cv2.waitKey(1) & 0xFF == 32: #space key is pressed (add distance data to timeline)
+            if cv2.waitKey(1) & 0xFF == 32: 
               cv2.putText(image,  "Recording", (50,50), 0, 0.5, 255)
               timeline.append(DistanceData)
 
-            if len(timeline) > 100: #given 100 frames of data, save to pickle and release
+            if len(timeline) > 100: 
                 with open(PKL_PATH, 'wb') as f:
                     pickle.dump(timeline, f)
                 break
 
             cv2.imshow("MediaPipe Hands", image)
-            if cv2.waitKey(5) & 0xFF == 27: #press escape to break
+            if cv2.waitKey(5) & 0xFF == 27:
                 break
             
     cap.release()
